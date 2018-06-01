0ddf9b64a420513ce6ac7b689936488ef12323c8
Enzo Soulié <soulie.enzo@gmail.com>
2018-06-01 12:55:20 +0200

    Add remove music  action

--- src/components/music/Music.js
+++ src/components/music/Music.js
@@ -4,14 +4,14 @@ import Table from 'components/base/Table';
 
 class Music extends PureComponent {
   render() {
-    const { playlist, myPlaylist, addMusic } = this.props;
+    const { playlist, myPlaylist, addMusic, removeMusic } = this.props;
     const headers = ['Titre de la chanson', 'Jouer la musique', 'Playlist action']
 
     return (
       <div className='container'>
         <h1>Music Page</h1>
         <Table headers={headers} playlist={playlist} playlistAction={addMusic}/>
-        <Table headers={headers} playlist={myPlaylist} />
+        <Table headers={headers} playlist={myPlaylist} playlistAction={removeMusic}/>
       </div>
     );
   }
